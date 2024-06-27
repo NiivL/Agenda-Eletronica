0f9f9423f14d846a3b3eca86264e21baf2ba7ec5
Nivaldo Lopes <nivaldolsrj@gmail.com>
2024-06-27 09:38:20 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda Eletrônica</title>
+</head>
+<body>
+    <h1>Agenda Eletrônica</h1>
+    
+    <h2>Adicionar Contato</h2>
+    <form method="post">
+        Nome: <input type="text" name="nome"><br>
+        Telefone: <input type="text" name="telefone"><br>
+        Email: <input type="email" name="email"><br>
+        <input type="submit" name="enviarDados">
+    </form>
+    
+    <h2>Contatos</h2>
+</body>
+</html>
