8a18c8ce2b81db20f10165c56fc8aafeb46a10a9
Nivaldo Lopes <nivaldolsrj@gmail.com>
2024-06-27 09:57:26 -0300

    adicionando a tela de login

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda Eletrônica</title>
+    <link rel="stylesheet" href="reset.css">
 </head>
 <body>
     <h1>Agenda Eletrônica</h1>
     
     <h2>Adicionar Contato</h2>
-    <form method="post">
+    <form class="" method="post">
         Nome: <input type="text" name="nome"><br>
         Telefone: <input type="text" name="telefone"><br>
         Email: <input type="email" name="email"><br>
---
+++ telaLogin.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="telaLogin.css">
+    <link rel="stylesheet" href="reset.css">
+</head>
+<body>
+    <div>
+        <h1>Bem vindo</h1>
+        <h3>Acesse sua conta agora</h3>
+        <a href="#">Entrar</a>
+
+    </div>
+    
+</body>
+</html>
