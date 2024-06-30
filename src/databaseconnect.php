3191f3ffea89115e7b0280c08f3e97995192da00
Nivaldo Lopes <nivaldolsrj@gmail.com>
2024-06-30 17:33:38 -0300

    adicionado uma condição de cadastro

--- src/databaseconnect.php
+++ src/databaseconnect.php
@@ -1,17 +1,27 @@
 <?php
 require_once 'pdoconfig.php';
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username,
-$password);
+    $conn = new PDO(
+        "mysql:host=$host;dbname=$dbname",
+        $username,
+        $password
+    );
 
     // $id = $_POST["campo1"];
     $user = $_POST["usuario"];
     $senha = $_POST["senha"];
 
-    $sql = "INSERT INTO pessoas SET user='$user', password='$senha'";
-    $sql = $conn->query($sql);
 
-    header("Location: home.php");
+    if ($user == '' || $senha == '') {
+        echo "<script type='javascript'>alert('Por favor preencha todos os dados!');";
+        header("Location: telacadastro.php");
+
+    } else {
+        $sql = "INSERT INTO pessoas SET user='$user', password='$senha'";
+        $sql = $conn->query($sql);
+
+        header("Location: home.php");
+    }
 
     // echo "Conectado a $dbname em $host com sucesso.";
 } catch (PDOException $pe) {
